8be84c56cd4df8df558ca53e8708b17a03a912ec
SamSMorales <silva.moralesSSM11@gmail.com>
2022-11-10 12:23:14 -0500

    Proyecto Spring
    Proyecto Api Rest Spring

---
+++ src/main/java/pe/com/escuelanueva/entity/ParentescoEntity.java
@@ -0,0 +1,41 @@
+
+package pe.com.escuelanueva.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//LIBRERIAS PARA OPTIMIZAR CÓDIGO
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity (name="ParentescoEntity")
+@Table(name="parentesco")
+
+public class ParentescoEntity  implements Serializable {
+    
+    //Serializado
+    private static final long serialVersionUID =1L;
+    
+    //Atributos
+    @Id
+    @Column(name="codpar")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+    
+    @Column(name="despar")
+    private String descripcion;
+    
+    @Column(name="estpar")
+    private boolean estado;
+           
+}
